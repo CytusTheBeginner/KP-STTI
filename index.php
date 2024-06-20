fca570a5da80b31c7d1b6b7c823691340b9211f1
hendri <hendri240xie@gmail.com>
2024-06-20 18:59:14 +0700

    index.php done

--- index.php
+++ index.php
@@ -1,36 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Landing Page</title>
-    <link rel="stylesheet" href="styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <title>Landing Page</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand fw-bold" href="#">
-                <img src="./img/logo.png" alt="Logo" width="120" height="96" class="d-inline-block align-text-center">
-                <span class="fs-3 pt-3 text-primary">SMAS</span> MAIREYAWIRA
-            </a>
-            <a href="login.php" class="btn btn-primary btn-lg mx-3">Login</a>
-        </div>
-    </nav>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand fw-bold" href="#">
+        <img src="./img/logo.png" alt="Logo" width="90" height="70" class="d-inline-block align-text-center">
+        <span class="fs-3 pt-3 text-primary">SMAS</span> MAIREYAWIRA
+      </a>
+      <a href="login.php" class="btn btn-primary btn-lg mx-3">Login</a>
+    </div>
+  </nav>
 
-    <section class="container">
-        <div class="container overflow-hidden text-center">
-            <div class="row gx-5">
-                <div class="col">
-                    <div class="p-3 align-baseline mt-5"><h1>Selamat Datang di Website Aplikasi Kas Organisasi SMAS Maitreyawira Tanjungpinang.</h1></div>
-                </div>
-                <div class="col">
-                    <div class="p-3"><img src="./img/kompo.png" alt=""></div>
-                </div>
-            </div>
-        </div>
-        <h1></h1>
-    </section>
+  <section>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 d-flex align-items-center">
+        <h1>Selamat datang di Website Aplikasi Kas SMA Maitreyawira Tanjungpinang.</h1>
+      </div>
+      <div class="col-lg-6 mb-3">
+      <img src="./img/kompo.png" class="img-fluid border-0">
+      </div>
+    </div>
+  </div>
+  </section>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <footer class="bg-primary fixed-bottom">
+      <p class="mt-3 text-center text-light fw-bold">Copyright 2025</p>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
 </body>
+
 </html>
